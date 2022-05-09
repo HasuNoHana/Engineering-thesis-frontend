6f36034a02a733bba4512d3afc9887b050e310b3
Zuzanna Santorowska <zuzannasantorowska@gmail.com>
2022-05-09 09:45:48 +0200

    remove unused imports

--- src/app/config/config.service.ts
+++ src/app/config/config.service.ts
@@ -1,7 +1,5 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable, throwError } from 'rxjs';
-import { catchError, retry } from 'rxjs/operators';
 
 @Injectable()
 export class ConfigService {
