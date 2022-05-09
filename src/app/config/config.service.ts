5912a3b8846800682f4ce54a012b543a4c05c7e2
Zuzanna Santorowska <zuzannasantorowska@gmail.com>
2022-05-09 09:54:11 +0200

    comment out unused Config Service

--- src/app/config/config.service.ts
+++ src/app/config/config.service.ts
@@ -1,22 +1,22 @@
-import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-
-@Injectable()
-export class ConfigService {
-  constructor(private http: HttpClient) { }
-
-  configUrl = 'assets/config.json';
-
-  getConfig() {
-    return this.http.get<Config>(this.configUrl);
-  }
-
-  showConfig() {
-    this.configService.getConfig()
-      .subscribe((data: Config) => this.config = {
-        heroesUrl: data.heroesUrl,
-        textfile:  data.textfile,
-        date: data.date,
-      });
-  }
-}
+// import { Injectable } from '@angular/core';
+// import { HttpClient } from '@angular/common/http';
+//
+// @Injectable()
+// export class ConfigService {
+//   constructor(private http: HttpClient) { }
+//
+//   configUrl = 'assets/config.json';
+//
+//   getConfig() {
+//     return this.http.get<Config>(this.configUrl);
+//   }
+//
+//   showConfig() {
+//     this.configService.getConfig()
+//       .subscribe((data: Config) => this.config = {
+//         heroesUrl: data.heroesUrl,
+//         textfile:  data.textfile,
+//         date: data.date,
+//       });
+//   }
+// }
