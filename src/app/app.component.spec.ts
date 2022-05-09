f7aa329057dddd833840325f1b776227a8e6061d
Zuzanna Santorowska <zuzannasantorowska@gmail.com>
2022-05-09 09:51:01 +0200

    fix tests

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -26,7 +26,7 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('common_frontend app is running!');
+    expect(compiled.querySelector('h1')?.textContent).toContain('common_frontend');
   });
-  
+
 });
