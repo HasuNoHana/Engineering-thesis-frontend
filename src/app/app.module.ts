a0f75b2d6558d37a267e86d132f9ff472f46e393
Zuzanna Santorowska <zuzannasantorowska@gmail.com>
2022-05-09 11:09:30 +0200

    add simple task list

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,8 @@ import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
 import { TasksComponent } from './tasks/tasks.component';
 import { TaskListComponent } from './tasks/task-list/task-list.component';
-import { TaskItemComponent } from './recipes/task-list/task-item/task-item.component';
+import {TaskItemComponent} from "./tasks/task-list/task-item/task-item.component";
+import {AppRoutingModule} from "./app-routing.module";
 
 
 @NgModule({
@@ -17,7 +18,8 @@ import { TaskItemComponent } from './recipes/task-list/task-item/task-item.compo
   ],
   imports: [
     BrowserModule,
-    HttpClientModule
+    HttpClientModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/tasks/task-list/task-item/task-item.component.ts
+++ src/app/tasks/task-list/task-item/task-item.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
+import {Task} from '../../task.model';
 
 @Component({
   selector: 'app-task-item',
@@ -6,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./task-item.component.css']
 })
 export class TaskItemComponent implements OnInit {
+  @Input('taskItem') task: Task;
+  @Input() index: number;
 
   constructor() { }
 
