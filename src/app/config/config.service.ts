be90fd46345492ed009acae29fb61c5acc5aedb6
Zuzanna Santorowska <zuzannasantorowska@gmail.com>
2022-05-09 09:43:01 +0200

    start after course

---
+++ src/app/config/config.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable, throwError } from 'rxjs';
+import { catchError, retry } from 'rxjs/operators';
+
+@Injectable()
+export class ConfigService {
+  constructor(private http: HttpClient) { }
+
+  configUrl = 'assets/config.json';
+
+  getConfig() {
+    return this.http.get<Config>(this.configUrl);
+  }
+
+  showConfig() {
+    this.configService.getConfig()
+      .subscribe((data: Config) => this.config = {
+        heroesUrl: data.heroesUrl,
+        textfile:  data.textfile,
+        date: data.date,
+      });
+  }
+}
