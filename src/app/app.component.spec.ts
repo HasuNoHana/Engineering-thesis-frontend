264a422d1626541468436457fbd996619b64c50b
Zuzanna Santorowska <zuzannasantorowska@gmail.com>
2022-05-09 16:35:21 +0200

    delete automatically generated tests

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -16,17 +16,4 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'common_frontend'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('common_frontend');
-  });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('common_frontend');
-  });
-
 });
