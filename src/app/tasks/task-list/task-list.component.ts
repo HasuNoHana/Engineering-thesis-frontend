d12bd1d47463fb7a540edeb8a0c25fbdd29de4a3
Zuzanna Santorowska <zuzannasantorowska@gmail.com>
2022-05-10 12:59:45 +0200

    fix header look

--- src/app/tasks/task-list/task-list.component.ts
+++ src/app/tasks/task-list/task-list.component.ts
@@ -11,22 +11,32 @@ import {ActivatedRoute, Router} from "@angular/router";
 export class TaskListComponent implements OnInit, OnDestroy {
   tasks: Task[] = [];
   subTasks: Subscription;
+  isFetching = false;
 
   constructor(private taskService: TaskService,
               private router: Router,
               private route: ActivatedRoute) {}
 
   ngOnInit(): void {
-    this.tasks = this.taskService.getTasks();
-    this.subTasks = this.taskService.tasksChanged.subscribe(
-      (tasks: Task[]) => {
-        this.tasks = tasks;
-      }
-    );
+    this.fetchTasks();
+    // this.tasks = this.taskService.getTasks();
+    // this.subTasks = this.taskService.tasksChanged.subscribe(
+    //   (tasks: Task[]) => {
+    //     this.tasks = tasks;
+    //   }
+    // );
   }
 
   ngOnDestroy() {
     this.subTasks.unsubscribe();
   }
 
+  private fetchTasks() {
+    this.isFetching = true;
+    this.taskService.fetchTasks().subscribe((tasks: any) => {
+      console.log(tasks);
+      this.tasks = tasks;
+      this.isFetching = false;
+    });
+  }
 }
--- src/app/tasks/task.service.ts
+++ src/app/tasks/task.service.ts
@@ -1,6 +1,6 @@
 import {Injectable} from "@angular/core";
 import {Task} from "./task.model";
-import {Subject} from "rxjs";
+import {map, Subject} from "rxjs";
 import {HttpClient, HttpHeaders, HttpParams} from "@angular/common/http";
 
 @Injectable({
@@ -9,19 +9,22 @@ import {HttpClient, HttpHeaders, HttpParams} from "@angular/common/http";
 export class TaskService{
 
   tasksChanged = new Subject<Task[]>();
-  private tasks: Task[] = [
-    new Task(1,'zmywanie'),
-    new Task(2,'pranie')
-  ];
+  private tasks: Task[];
 
   constructor(private http: HttpClient) {
   }
 
-  getTasks(){
-    this.http.get<{ [key: number]: Task }>('http://localhost:8080/api/tasks',
-    ).subscribe((tasks: any) => {
-      console.log(tasks);
-    });
-    return this.tasks.slice();
+  fetchTasks(){
+    return this.http.get<{ [key: string]: Task }>('http://localhost:8080/api/tasks')
+      .pipe(map(responseData => {
+          const taskArray: Task[] = [];
+          console.log(responseData);
+          console.log(responseData[0]);
+          // for (const key in responseData) {
+          //   taskArray.push( ...responseData[key]);
+          // }
+        // return taskArray;
+        return responseData;
+      }));
   }
 }
