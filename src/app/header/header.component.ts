1e66e7b769d39991bb439cea9203ea16b4056a3c
Zuzanna Santorowska <zuzannasantorowska@gmail.com>
2022-05-16 10:23:04 +0200

    header works

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,9 +1,17 @@
-import {Component} from '@angular/core';
+import {Component, ElementRef, Renderer2} from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html'
 })
 export class HeaderComponent {
+  isOpen = false;
 
+  constructor(private elRef: ElementRef) {
+  }
+
+  showDropdown() {
+    this.isOpen = !this.isOpen;
+
+  }
 }
