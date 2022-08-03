f1aac298cf30c326afe3a117496bc771408f89a3
Zuzanna Santorowska <zuzannasantorowska@gmail.com>
2022-08-03 11:31:32 +0200

    add room in task

--- src/app/tasks/task.model.ts
+++ src/app/tasks/task.model.ts
@@ -1,15 +1,18 @@
+import {Room} from "../rooms/room.model";
 
 export class Task {
 
   public id: number;
   public name: string;
   public price: number;
+  public room: Room;
   public done: boolean;
 
-  constructor(id: number, name: string, price: number, done: boolean) {
+  constructor(id: number, name: string, price: number, room: Room, done: boolean) {
     this.id = id;
     this.name = name;
     this.price = price;
+    this.room = room;
     this.done = done;
   }
 }
