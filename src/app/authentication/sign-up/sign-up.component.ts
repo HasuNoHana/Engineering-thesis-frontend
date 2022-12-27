82490dc69a83bf768943659e16875427aed78adc
Zuzanna Santorowska <zuzannasantorowska@gmail.com>
2022-12-27 18:41:23 +0100

    add sign up

--- src/app/authentication/sign-up/sign-up.component.ts
+++ src/app/authentication/sign-up/sign-up.component.ts
@@ -1,8 +1,32 @@
 import {Component} from '@angular/core';
+import {AuthenticationService} from '../authentication.service';
+import {HttpClient} from '@angular/common/http';
+import {Router} from '@angular/router';
+import {UserSignup} from "./userSignup.model";
 
 @Component({
   selector: 'app-sign-up',
   templateUrl: './sign-up.component.html'
 })
 export class SignUpComponent {
+
+
+  credentials = {username: '', password: '', passwordConfirmation: '', houseJoinCode: ''};
+
+
+  constructor(public authService: AuthenticationService,
+              private http: HttpClient,
+              private router: Router) {
+  }
+
+  signup() {
+
+    // TODO validate credentials (check passowrd confirmation)
+
+    let userSignup: UserSignup = new UserSignup(this.credentials.username, this.credentials.password, this.credentials.houseJoinCode);
+
+    this.http.post('api/create-user', userSignup).subscribe(() => {
+      this.router.navigateByUrl('/login');
+    });
+  }
 }
