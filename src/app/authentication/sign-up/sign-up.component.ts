8095626c416c0f164924b9eb89cc8fad7279cd9f
Zuzanna Santorowska <zuzannasantorowska@gmail.com>
2023-01-04 11:00:37 +0100

    wip2

--- src/app/authentication/sign-up/sign-up.component.ts
+++ src/app/authentication/sign-up/sign-up.component.ts
@@ -14,11 +14,65 @@ export class SignUpComponent {
   credentials = {username: '', password: '', passwordConfirmation: '', houseJoinCode: ''};
   houseCheckbox = false;
   badJoinCode = false;
+  passwordsDiffer = false;
+  badRequest = false;
+  // signUpForm: FormGroup;
 
   constructor(public authService: AuthenticationService,
               private http: HttpClient,
               private router: Router) {
   }
+
+  // ngOnInit() {
+  //   this.createForm();
+  // }
+  //
+  // createForm() {
+  //   this.signUpForm = new FormGroup({
+  //     'username': new FormControl("", [Validators.required]),
+  //     'password': new FormControl("", [Validators.required]),
+  //     'passwordConfirmation': new FormControl("", [Validators.required]),
+  //     'houseJoinCode': new FormControl("", [])
+  //   });
+  //   console.log("ji")
+  // }
+
+  // signup() {
+  //   this.badJoinCode = false;
+  //
+  //   // TODO validate credentials (check passowrd confirmation)
+  //   if(this.houseCheckbox) {
+  //     if(this.signUpForm.controls['houseJoinCode'].value === '') {
+  //       this.badJoinCode = true;
+  //       return;
+  //     }
+  //     let userSignup: UserSignup = new UserSignup(this.signUpForm.controls['username'].value,
+  //       this.signUpForm.controls['password'].value, this.signUpForm.controls['houseJoinCode'].value);
+  //
+  //     this.http.post('api/createUserForExistingHouse', userSignup).subscribe(() => {
+  //       this.badRequest = false;
+  //       this.router.navigateByUrl('/login');
+  //     }, error => {
+  //       if(error.status === 400) {
+  //         console.log(error);
+  //         this.badRequest = true;
+  //       }
+  //     });
+  //
+  //   } else {
+  //     let user = {"username": this.signUpForm.controls['username'].value, "password": this.signUpForm.controls['password'].value};
+  //
+  //     this.http.post('api/createUserAndHouse', user).subscribe(() => {
+  //       this.router.navigateByUrl('/login');
+  //     }, error => {
+  //       if(error.status === 400) {
+  //         console.log(error);
+  //         this.badRequest = true;
+  //       }
+  //     });
+  //   }
+  //
+  // }
 
   signup() {
     this.badJoinCode = false;
@@ -29,17 +83,29 @@ export class SignUpComponent {
         this.badJoinCode = true;
         return;
       }
-      let userSignup: UserSignup = new UserSignup(this.credentials.username, this.credentials.password, this.credentials.houseJoinCode);
+      let userSignup: UserSignup = new UserSignup(this.credentials.username,
+        this.credentials.password, this.credentials.houseJoinCode);
 
-      this.http.post('api/create-user', userSignup).subscribe(() => {
+      this.http.post('api/createUserForExistingHouse', userSignup).subscribe(() => {
+        this.badRequest = false;
         this.router.navigateByUrl('/login');
+      }, error => {
+        if(error.status === 400) {
+          console.log(error);
+          this.badRequest = true;
+        }
       });
 
     } else {
-      let userSignup: UserSignup = new UserSignup(this.credentials.username, this.credentials.password, "");
+      let user = {"username": this.credentials.username, "password": this.credentials.password};
 
-      this.http.post('api/create-user', userSignup).subscribe(() => {
+      this.http.post('api/createUserAndHouse', user).subscribe(() => {
         this.router.navigateByUrl('/login');
+      }, error => {
+        if(error.status === 400) {
+          console.log(error);
+          this.badRequest = true;
+        }
       });
     }
 
